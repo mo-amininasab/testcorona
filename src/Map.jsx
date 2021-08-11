744ded37e18385996a868d6c22755136fa44ae88
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-08-12 02:50:09 +0430

    simple map added.

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
 function Map() {
     return (
-        <div className="map">
-           <h1>i am map</h1>
-        </div>
+        <div>
+        <ComposableMap>
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
+            }
+          </Geographies>
+        </ComposableMap>
+      </div>
     )
 }
 
